b839f234e1148eec865cb9346c4903b4093451f6
MahatmaK <nirajkhatri21@gmail.com>
2021-06-18 15:30:43 -0400

    Completed date search. Ready to start bonus

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,43 +6,84 @@ var tableData = data;
 // Select and store tbody as variable
 var tbody = d3.select('tbody');
 
-tableData.forEach(function(ufoInstance) {
+tableData.forEach((ufoInstance) => {
     // console.log(ufoInstance);
     var row = tbody.append('tr');
 
-    Object.entries(ufoInstance).forEach(function([key, value]) {
+    Object.entries(ufoInstance).forEach(([key, value]) => {
         // console.log(value);
         var cell = row.append('td');
         cell.text(value);
     });
 });
 
+console.log('test');
+
+// CLICK AND ENTER FILTER
+// ----------------------------------------- //
+
+// Capture input tag
+var form = d3.select("form");
+
+// If the user clicks outside the input box
+form.on("submit", dateFilter);
+
+form.on("change", dateFilter);
+
+// Caputure button tag
+var button = d3.select('button');
+
+// If the user clicks the button
+button.on("click", dateFilter);
 
 
+// FUNCTIONS
+// ----------------------------------------- //
+
+// Filter function
+function dateFilter () {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Store user input
+    var userDate = d3.select("#datetime");
+    var userValue = userDate.property("value")
+
+    // Delete all 'tr' and 'td' tags
+    tbody.remove();
+
+    // Add back tbody in proper location
+    var table = d3.select('table');
+    tbody = table.append('tbody');
 
 
+    console.log(userValue);
+    
+    // Iterate through data and pull values with matching date/time
+    if (userValue == "") {
+        tableData.forEach((ufoInstance) => {
+            
+            var row = tbody.append('tr');
+        
+            Object.entries(ufoInstance).forEach(([key, value]) => {
+                var cell = row.append('td');
+                cell.text(value);
+            });
+        });
+    }
 
+    else {
+        var filteredData = data.filter(ufoSighting => ufoSighting.datetime === userValue);     
+        
+        filteredData.forEach((ufoInstance) => {
+            
+            var row = tbody.append('tr')
 
-
-
-
-
-
-
-// Store date
-var date = tableData.map(sighting => sighting.datetime);
-
-// Store city
-var city = tableData.map(sighting => sighting.city);
-
-// Store state
-var cstate = tableData.map(sighting => sighting.state);
-
-// Store country
-var country = tableData.map(sighting => sighting.country);
-
-// Store shape
-var shape = tableData.map(sighting => sighting.shape);
-
-// Store comments
-var comments = tableData.map(sighting => sighting.comments);
+            Object.entries(ufoInstance).forEach(([key, value]) => {
+                var cell = row.append('td');
+                cell.text(value);
+            });
+        });
+    }       
+}
