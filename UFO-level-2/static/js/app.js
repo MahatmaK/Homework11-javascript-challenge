1de6077d01976913c707fb9e23045d220089c969
MahatmaK <nirajkhatri21@gmail.com>
2021-06-21 14:37:26 -0400

    Added reset button. Need to clean up comments in code.

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -6,16 +6,8 @@ var tableData = data;
 // Select and store tbody as variable
 var tbody = d3.select('tbody');
 
-tableData.forEach((ufoInstance) => {
-    // console.log(ufoInstance);
-    var row = tbody.append('tr');
-
-    Object.entries(ufoInstance).forEach(([key, value]) => {
-        // console.log(value);
-        var cell = row.append('td');
-        cell.text(value);
-    });
-});
+// Load data on window load
+window.onload = loadData();
 
 
 // CLICK AND ENTER FILTER
@@ -29,15 +21,61 @@ form.on("submit", massFilter);
 
 // form.on("change", massFilter);
 
-// Capture button tag
-var button = d3.select('button');
+// Capture Filter Table button tag
+var filterButton = d3.select('button');
 
-// If the user clicks the button
-button.on("click", massFilter);
+// If the user clicks the filter table button
+filterButton.on("click", massFilter);
 
+// Capture Reset Table button tag
+var allButtons = d3.selectAll('button');
+var resetButton = d3.select(allButtons._groups[0][1]);
+
+console.log(resetButton);
+
+// If the user clicks the reset table button
+resetButton.on("click", loadData);
 
 // FUNCTIONS
 // ----------------------------------------- //
+
+function loadData () {
+
+    // CLEAR FILTER SEARCH
+    // --------------------------//
+    // Select all input tags
+    var userData = d3.selectAll('#datetime')
+        
+        // Iterate through each tag
+        .each(function(d,i) {
+
+            // Clear any old filters
+            this.value = '';
+            
+        });
+    
+
+    // 
+    // Delete all 'tr' and 'td' tags
+    tbody.remove();
+
+    // Add back tbody in proper location
+    var table = d3.select('table');
+    tbody = table.append('tbody');
+
+    tableData.forEach((ufoInstance) => {
+        // console.log(ufoInstance);
+        var row = tbody.append('tr');
+
+        Object.entries(ufoInstance).forEach(([key, value]) => {
+            // console.log(value);
+            var cell = row.append('td');
+            cell.text(value);
+        });
+    });
+
+}
+
 
 function massFilter () {
 
